32d491c5da73c6d9efb36b273c2457baf1d67507
Sara Leya <kolesnichenkova_@mail.ru>
2023-01-22 18:31:15 +0300

    modified:   practic.html modified:   script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-confirm('fuck');
+let n = 1;
+n + 5;
+n — 3;
+n * 7;
+n / 3;
+n++;
+n--;
+alert (n);
+
+
+let x = 2;
+let result = Math.pow (x,5);
+alert(result);
+
+let d = 10;
+let g = 2;
+alert(d+g);
+alert(d-g);
+alert(d*g);
+alert(d/g);
+
+let n = 'Джеймс Гослинг';
+alert (n);
+
+let year = 2007;
+alert(year);
+
+let ab = 10;
+alert(ab);
+
+
+
+
+
+

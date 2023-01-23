853f13fdbbd48c100761d3782a96961ec050e265
Sara Leya <kolesnichenkova_@mail.ru>
2023-01-23 19:49:26 +0300

    modified:   script.js

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
+let age = prompt('Сколько Вам лет?');
+const user {
+name = 'Sara',
+age = 31,
+isAdmin = false,
+};
+user['city of residence'] = 'Mexico';
+user.age = 43;
+user.dog = mops;
+delete user['city of residence'];
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+console.log(user[info]);
+alert(user[info]);
+
+
 let n = 1;
-n + 5;
-n — 3;
-n * 7;
-n / 3;
+n+5;
+n—3;
+n*7;
+n/3;
 n++;
 n--;
 alert (n);
 
+let z = 9;
+let b = 2;
+alert(z%b);
 
 let x = 2;
-let result = Math.pow (x,5);
-alert(result);
+x**5;
+alert(x);
 
 let d = 10;
 let g = 2;
@@ -27,7 +45,6 @@ alert(year);
 
 let ab = 10;
 alert(ab);
-
 
 
 

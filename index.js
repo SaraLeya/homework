890d3c4545c5462dcb2e2de0c825363385331352
Sara Leya <kolesnichenkova_@mail.ru>
2023-02-02 19:52:54 +0300

    modified:   _mobile.scss new file:   _seasons.scss deleted:    hw4.css deleted:    hw4.css.map deleted:    hw4.html deleted:    hw4.scss new file:   index.html new file:   index.js deleted:    leson2&3.js renamed:    index1.html -> seasons.html renamed:    style1.css -> style.css new file:   style.css.map renamed:    style1.scss -> style.scss deleted:    style1.css.map

---
+++ index.js
@@ -0,0 +1,24 @@
+/*Задание 3*/
+
+
+/*Задание 2*/
+let EvenNumber =(n) => {
+    if ( n % 2 == 0){
+        alert(`Число четное`);
+    } else{
+        alert(`Число нечетное`);
+    }
+}
+
+EvenNumber(4);
+
+/*Задание 1*/
+let bigest =(a,b) => {
+    if ( a > b){
+        alert(b);
+    } else{
+        alert(a);
+    }
+}
+
+bigest(7, 2);

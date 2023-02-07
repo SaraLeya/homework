d9c57f99c04ac679ee1e708694db976e9695ee17
SaraLeya <kolesnichenkova_@mail.ru>
2023-02-07 12:25:01 +0300

    deleted:    leson2.js modified:   script.js

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
-let age = prompt('Сколько Вам лет?');
-const user {
-name = 'Sara',
-age = 31,
-isAdmin = false,
-};
-user['city of residence'] = 'Mexico';
-user.age = 43;
-user.dog = mops;
-delete user['city of residence'];
-let info = prompt("Какую информацию хотите узнать о пользователе?");
-console.log(user[info]);
-alert(user[info]);
+/*Урок 2.2 */
+/*Задание 1*/
+let ab = 10;
+alert(ab);
 
+/*Задание 2*/
+let year = 'Год выпуска первого айфона 2007';
+alert(year);
 
-let n = 1;
-n+5;
-n—3;
-n*7;
-n/3;
-n++;
-n--;
+/*Задание 3*/
+let n = 'Создатель проклятого JS - Джеймс Гослинг';
 alert (n);
 
-let z = 9;
-let b = 2;
-alert(z%b);
-
-let x = 2;
-x**5;
-alert(x);
-
+/*Задание 4*/
 let d = 10;
 let g = 2;
 alert(d+g);
@@ -37,16 +19,41 @@ alert(d-g);
 alert(d*g);
 alert(d/g);
 
-let n = 'Джеймс Гослинг';
-alert (n);
+/*Задание 5*/
+let x = 2;
+x**5;
+alert(x**5);
 
-let year = 2007;
-alert(year);
+/*Задание 6*/
+let z = 9;
+let b = 2;
+alert(z%b);
 
-let ab = 10;
-alert(ab);
+/*Задание 7*/
+let num = 1;
+num +=5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
 
+/*Задание 8*/
+let age = prompt('Сколько Вам лет?');
+alert(age);
 
-
-
-
+/*Задание 9*/
+const user {
+    name: 'Sara',
+    age: 31,
+    isAdmin:false,
+    }
+    user['city of residence'] = 'Mexico';
+    user.age = 43;
+    user.dog = mops;
+    delete user['city of residence'];
+    let info = prompt("Какую информацию хотите узнать о пользователе?");
+    console.log(user[info]);
+    alert(user[info]);
+ 
